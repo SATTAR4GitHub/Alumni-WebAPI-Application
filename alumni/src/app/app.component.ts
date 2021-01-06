1d3bab51f517d58e48de05087e335ad1758c59b3
Abdus Sattar Mia <000394648@mohawkcollege.ca>
2021-01-06 09:28:15 -0500

    Angular project changes

--- alumni/src/app/app.component.ts
+++ alumni/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +14,19 @@ export class AppComponent implements OnInit {
 
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private accountService: AccountService) {}
 
   ngOnInit() {
    this.getUsers();
+   this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      this.accountService.setCurrentUser(user);
+    }
+
   }
 
   getUsers() {
